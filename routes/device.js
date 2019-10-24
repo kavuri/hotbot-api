bd41ce588dd2995b5f7bdefb5c1d27f103a0f678
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2019-10-24 21:02:03 +0530

    Added device APIs
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- routes/device.js
+++ routes/device.js
@@ -15,21 +15,27 @@
  var router = express.Router();
 
  const DeviceModel = require('../../src/db').DeviceModel,
+       HotelModel = require('../../src/db').HotelModel,
        _ = require('lodash'),
        { check, validationResult } = require('express-validator');
 
-router.get('/', function(req, res) {
-    console.log('get /device');
-    res.send('get /device');
+router.get('/', async function(req, res) {
+    console.log('get all devices');
+    try {
+        let devices = await DeviceModel.find({status:'inactive'}).sort({last_reset: -1}).exec();
+        console.log(devices);
+        return res.status(200).send(devices);
+    } catch (error) {
+        console.log('error in getting all devices.', error);
+        return res.status(400).send(error);
+    }
 });
 
 router.post('/', [
     check('device_id').exists(),
-    check('hotel_id').exists(),
     check('user_id').exists(),
+    check('hotel_id').exists(),
     check('room').exists(),
-    check('coordinates').exists(),
-    check('address').exists(),
     check('status').exists()
 ], async function(req, res, next) {
     
@@ -38,24 +44,79 @@ router.post('/', [
     } catch (error) {
         return res.status(422).send(error);
     }
-console.log(req.body.device_id);
+    
+    // console.log(req.body.device_id);
     try {
         let device = new DeviceModel({
             device_id: req.body.device_id,
             hotel_id: req.body.hotel_id,
             user_id: req.body.user_id,
             room: req.body.room,
-            address: req.body.address,
-            coordinates: req.body.coordinates,
-            status: req.body.status
+            status: 'inactive' // status would always be inactive when created,
         });
 
-        console.log('==device=',device)
+        // console.log('==device=',device);
         let result = await device.save();
-        console.log('@@@',result)
+        // console.log('@@@', result);
         res.status(200).send(result);
     } catch (error) {
         console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+router.put('/:device_id/activate', [
+    check('device_id').exists()
+], async function(req, res, next) {
+    console.log('activating device ' + req.params.device_id);
+
+    let device_id = req.params.device_id;
+    try {
+        await DeviceModel.updateOne({device_id: device_id}, { $set: { status: 'active' } });
+        console.log('device activated.');
+        return res.status(200).send({device_id: device_id, activated: true});
+    } catch (error) {
+        console.log('error in activating device.', error);
+        res.status(500).send(error);
+    }
+});
+
+router.put('/:device_id/deactivate', [
+    check('device_id').exists()
+], async function(req, res, next) {
+    console.log('de-activating device ' + req.params.device_id);
+
+    let device_id = req.params.device_id;
+    try {
+        await DeviceModel.updateOne({device_id: device_id}, { $set: { status: 'inactive' } });
+        console.log('device deactivated.');
+        return res.status(200).send({device_id: device_id, deactivated: true});
+    } catch (error) {
+        console.log('error in deactivating device.', error);
+        res.status(500).send(error);
+    }
+});
+
+router.put('/:device_id/addToHotel', [
+    check('device_id').exists(),
+    check('hotel_id').exists()
+], async function(req, res) {
+    console.log('adding device ' + req.params.device_id + ' to hotel ' + req.query.hotel_id);
+
+    let device_id = req.params.device_id, hotel_id = req.query.hotel_id;
+
+    try {
+        // Get hotel object
+        let hotel = await HotelModel.findOne({hotel_id: hotel_id}).exec();
+        if (_.isNull(hotel) || _.isUndefined(hotel)) {
+            return res.status(400).send('hotel with id ' + hotel_id + ' not found');
+        }
+
+        // Update the device
+        await DeviceModel.updateOne({device_id: device_id}, { $set: { hotel_id: hotel_id, belongs_to: hotel } });
+        return res.status(200).send({device_id: device_id});
+    } catch (error) {
+        console.log('error in adding device to hotel.', error);
         res.status(500).send(error);
     }
 });
