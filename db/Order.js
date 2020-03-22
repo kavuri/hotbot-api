41d69c38ccf58cae0a3fd8c6d637611988e9e11f
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2020-03-22 20:27:50 +0530

    Updated Order schema
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- db/Order.js
+++ db/Order.js
@@ -9,14 +9,21 @@ const mongoose = require('mongoose'),
     DBConn = require('./index').DBConn,
     AutoIncrement = require('./index').AutoIncrement;
 
+/**
+ * new = for any new order, created by system
+ * progress = order fulfilment in progress, changed by hotel staff
+ * done = order fulfilment complete, changed by hotel staff
+ * cant_serve = hotel unable to serve order, changed by hotel staff
+ * cancelled = guest cancelled the order, changed by guest via bot
+ */
 var StatusSchema = new mongoose.Schema({
-    set_by: { type: String, required: true }, // This is the email id of the logged-in user
+    set_by: { type: String }, // This is the id of whoever changed the status
     status: { type: String, required: true, enum: ['new', 'progress', 'done', 'cant_serve', 'cancelled'], default: 'new' },
     created: { type: Date, default: Date.now }
 });
 
 var PrioritySchema = new mongoose.Schema({
-    set_by: { type: String, required: true }, // This is the email id of the logged-in user
+    set_by: { type: String }, // This is the email id of the logged-in user
     priority: { type: String, required: true, enum: ['urgent', 'asap', 'leisure'], default: 'asap' },
     created: { type: Date, default: Date.now }
 });
@@ -38,9 +45,13 @@ var OrderSchema = new mongoose.Schema({
     hotel_id: { type: String, required: true, index: true }, // this is the "address1" field
     user_id: { type: String, required: true },
     room_no: String,
-    items: { type: [OrderItemsSchema], required: true },
+    item: { type: OrderItemsSchema, required: true },
+    order_group_id: { type: String, required: true },
+    ///items: { type: [OrderItemsSchema], required: true },
     priority: { type: [PrioritySchema], default: { priority: 'asap' } },
     status: { type: [StatusSchema], default: { status: 'new' } },
+    curr_priority: { type: PrioritySchema, required: true, default: { status: 'new' } },
+    curr_status: { type: StatusSchema, required: true, default: { priority: 'asap' } },
     completion_time: Date,
     cancelled_by: String,
     comments: { type: [CommentSchema] }
@@ -56,8 +67,11 @@ OrderSchema.post('save', async function (doc) {
     // This is useful for checking if the guest has ordered the same item again
     console.log('Updating orders to room');
     const filter = { hotel_id: doc.hotel_id, room_no: doc.room_no, checkout: null };
-    let room = await CheckinCheckoutModel.findOne(filter).exec();
-    room.orders.push(doc);
+    try {
+        await CheckinCheckoutModel.findOneAndUpdate(filter, { $push: { orders: doc } }, { upsert: true }).exec();
+    } catch (error) {
+        console.error('error in storing order reference to CheckinCheckout:', error);
+    }
     /*
     for (var i = 0; i < doc.items.length; i++) {
         let prevOrderIdx = _.findIndex(room.orders, { name: doc.items[i].name })
@@ -68,10 +82,11 @@ OrderSchema.post('save', async function (doc) {
         }
     }
     */
-
-    // Save the orders to the room
-    await room.save();
 });
+
+OrderSchema.pre('updateOne', { document: true, query: false }, function () {
+    this.set({ updated_at: new Date() });
+})
 
 OrderSchema.index({ hotel_id: 1, o_id: 1 }, { unique: true });
 OrderSchema.index({ hotel_id: 1, user_id: 1 });
